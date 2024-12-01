8289264f3bbcfb5a0419f487bc24bd714740fdcd
Ofir-Roz <ofir19923@gmail.com>
2024-12-01 14:00:32 +0200

    Add new about components - Team and Vision

---
+++ cmps/AboutCmps/Team.jsx
@@ -0,0 +1,14 @@
+
+
+export function Team() {
+    return (
+        <section>
+            <h2>Our Team</h2>
+            <ul>
+                <li>Ofir Rozanes - CEO </li>
+                <li>Jinni Tsafrir - Librarian</li>
+                <li>Roro Bingali - Marketer</li>
+            </ul>
+        </section>
+    )
+}
---
+++ cmps/AboutCmps/Vision.jsx
@@ -0,0 +1,14 @@
+
+export function Vision() {
+    return (
+        <section>
+            <h2>Vision</h2>
+            <ul>
+                <li>Make the world smarter</li>
+                <li>Sell as many books as we can</li>
+                <li>Make huge profits</li>
+                <li>Take over the world</li>
+            </ul>
+        </section>
+    )
+}
