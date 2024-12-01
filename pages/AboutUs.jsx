dc73cb958b23a65b44ad5c16a56480b2d467bd48
Ofir-Roz <ofir19923@gmail.com>
2024-12-01 14:02:12 +0200

    Fix the About page and add into it links for team and the vision comps

--- pages/AboutUs.jsx
+++ pages/AboutUs.jsx
@@ -1,11 +1,21 @@
+const { Outlet, Link, NavLink } = ReactRouterDOM
 
 export function AboutUs(){
 
     return (
-        <div>
-            <p>
-                bla bla bla
-            </p>
-        </div>
+        <section className="about-us">
+            <h1>About Books and Us...</h1>
+            <p>bla bla bla</p>
+            <section>
+                <nav>
+                    <NavLink to="/about/team">Team </NavLink>| 
+                    <NavLink to="/about/vision"> Vision</NavLink>
+                </nav>
+            </section>
+
+            <section>
+                <Outlet />
+            </section>
+        </section>
     )
 }
