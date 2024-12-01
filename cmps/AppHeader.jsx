fb70998691a3ba2b2ae84862e89ea4b5d80f9dc3
Ofir-Roz <ofir19923@gmail.com>
2024-12-01 12:41:04 +0200

    Small cosmetic changes

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -8,8 +8,8 @@ export function AppHeader() {
                 <h1>Miss Books</h1>
                 <nav className="app-nav">
                     <NavLink to="/home" >Home</NavLink>
-                    <NavLink to="/about">About</NavLink>
-                    <NavLink to="/book" >Books</NavLink>
+                    <NavLink to="/about">| About</NavLink>
+                    <NavLink to="/book" >| Explore Books</NavLink>
                 </nav>
             </section>
         </header>
