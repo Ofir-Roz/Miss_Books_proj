a9dd28a2a58890d7ecf166eb36b1201639df629a
Ofir-Roz <ofir19923@gmail.com>
2024-12-04 14:32:21 +0200

    Adjust BookIndex to send down callback to BookFilter to set the filter stats to show the right books

--- pages/BookIndex.jsx
+++ pages/BookIndex.jsx
@@ -6,24 +6,30 @@ const { useEffect, useState } = React
 
 export function BookIndex() {
     const [books, setBooks] = useState(null)
+    const [filterBy, setFilterBy] = useState(bookService.getDefaultFilter())
 
     useEffect(() => {
         loadBooks()
-    }, [])
+    }, [filterBy])
 
     function loadBooks() {
-        bookService.query()
+        bookService.query(filterBy)
             .then(setBooks)
             .catch(err => {
                 console.log("Problem getting books:", err)
             })
     }
 
+    function onSetFilter(filterBy) {
+        setFilterBy(prevFilter => ({
+            ...prevFilter, ...filterBy
+        }))
+    }
 
     if (!books) return <div>Loading...</div>
     return (
         <section className="book-index">
-            <BookFilter />
+            <BookFilter defaultFilter={filterBy} onSetFilter={onSetFilter} />
 
             <BookList books={books} />
         </section>
