d89ec47bd8fcb1dcfae1afb71f22246feded3248
Ofir-Roz <ofir19923@gmail.com>
2024-12-04 16:52:13 +0200

    Move to full data model with the provided demo function

--- cmps/BookPreview.jsx
+++ cmps/BookPreview.jsx
@@ -5,8 +5,8 @@ export function BookPreview({ book }) {
     return (
         <article className="book-preview">
             <h2>Title: {book.title}</h2>
-            <h2>Price: {book.listPrice.amount}</h2>
-            <img src={`../assets/img/BooksImages/${utilService.getRandomIntInclusive(1, 20)}.jpg`} alt="book-image" />
+            <h2>Price: {book.listPrice.amount} €</h2>
+            <img src={`../assets/img/BooksImages/${book.imgNum}.jpg`} alt="book-image" />
         </article>
     )
 
