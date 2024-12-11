2c598bf7cc51730f96e51826605e9f9b26df3180
Ofir-Roz <ofir19923@gmail.com>
2024-12-11 23:42:45 +0200

    Create functions to add next and prev bookId properties to the book object in order to link between books.

--- services/book.service.js
+++ services/book.service.js
@@ -39,6 +39,7 @@ function query(filterBy = {}) {
 
 function get(bookId) {
     return storageService.get(BOOK_KEY, bookId)
+        .then(_setNextPrevBookId)
 }
 
 function remove(bookId) {
@@ -65,6 +66,32 @@ function getEmptyBook(title = '', amount = 0, currencyCode = 'USD', isOnSale = f
 
 function getDefaultFilter() {
     return { title: '', minPrice: '', onSale: false, publishedYear: '' }
+}
+
+function isOnSale(isOnSale){
+    if (isOnSale)
+        return <h1 className={'on-sale-sign'}>On Sale!</h1>
+}
+
+function getPriceInColor(price){
+    let style
+    if (price > 150) style = {color: 'red'}
+    if (price < 20) style = {color: 'green'}
+    return style
+}
+
+/*--------------------------------------------------------------*/
+
+function _setNextPrevBookId(book) {
+    return query()
+        .then( books => {
+            const bookIdx = books.findIndex( currBook => currBook.id === book.id)
+            const nextBook = books[bookIdx + 1] ? books[bookIdx + 1] : books[0]
+            const prevBook = books[bookIdx - 1] ? books[bookIdx - 1] : books[books.length - 1]
+            book.nextBookId = nextBook.id
+            book.prevBookId = prevBook.id
+            return book
+        })
 }
 
 function _createBooks() {
@@ -99,16 +126,4 @@ function _createBooks() {
         books.push(book)
     }
     utilService.saveToStorage(BOOK_KEY, books)
-}
-
-function isOnSale(isOnSale){
-    if (isOnSale)
-        return <h1 className={'on-sale-sign'}>On Sale!</h1>
-}
-
-function getPriceInColor(price){
-    let style
-    if (price > 150) style = {color: 'red'}
-    if (price < 20) style = {color: 'green'}
-    return style
 }
